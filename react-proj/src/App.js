6cff16f6398ba4868df2d0e25960c1d901097605
kdang0 <73298064+kdang0@users.noreply.github.com>
2022-10-25 14:14:40 -0400

    react

---
+++ react-proj/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'; 
+
+class App extends Component {
+  render() {
+    return(
+      <>
+      <h1>Hello Dojo!</h1>
+      <p>Things I need to do:</p>
+      <ul>
+        <li>Learn React</li>
+        <li>Sleep</li>
+        <li>Run a marathon</li>
+        <li>Feed dogs</li>
+      </ul>
+      </>
+    );
+  }
+}
+
+export default App;
